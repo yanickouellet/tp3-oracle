711fe6ea69c21fec4cfca650de6a971a19990ac1
yanickouellet <yanickouellet555@gmail.com>
2014-12-06 13:59:01 -0500

    Generate classes and add some xhtml files

---
+++ src/com/dinfo/tp3/classes/HibernateUtil.java
@@ -0,0 +1,25 @@
+package com.dinfo.tp3.classes;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+	static {
+		try {
+			Configuration configuration = new Configuration().configure();
+			StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties());
+			sessionFactory = configuration.buildSessionFactory(builder.build());
+		} catch (Throwable ex) {
+			// Log the exception.
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
