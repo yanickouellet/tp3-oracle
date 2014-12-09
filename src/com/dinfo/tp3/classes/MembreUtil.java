810dda8a76b0cd142b3273a45ce4700aab6a493b
yanickouellet <yanickouellet555@gmail.com>
2014-12-09 12:00:13 -0500

    User login is working

--- src/com/dinfo/tp3/classes/MembreUtil.java
+++ src/com/dinfo/tp3/classes/MembreUtil.java
@@ -1,7 +1,10 @@
 package com.dinfo.tp3.classes;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -25,9 +28,15 @@ public class MembreUtil {
     public BiMembres GetMembreAuthentifie(String login, String motPasse)
     {
         BiMembres membre = null;
-    	
+        System.out.println(motPasse);
+    	System.out.println(sha256(motPasse));
         try {
-        	List<BiMembres> membres = session.createQuery("from BiMembres").list();
+        	List<BiMembres> membres = session
+        			.createQuery("from BiMembres where login=:login "
+        			+ "and motPasse=:motPasse")
+        			.setString("login", login)
+        			.setString("motPasse", sha256(motPasse))
+        			.list();
             
         	if (membres.size() > 0)
         		membre = membres.get(0);
@@ -39,5 +48,28 @@ public class MembreUtil {
         return membre;
     }
     
+    /**
+     * Inspiré de https://gist.github.com/avilches/750151
+     * @param valeur
+     * @return
+     */
+    private String sha256(String valeur) {
+    	MessageDigest md;
+    	String hash = "";
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+            md.update(valeur.getBytes("UTF-8"));
+            StringBuffer result = new StringBuffer();
+            for (byte byt : md.digest()) result.append(Integer.toString((byt & 0xff) + 0x100, 16).substring(1));
+            hash = result.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return "";
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return "";
+		} 
 
+    	return hash;
+    }
 }
